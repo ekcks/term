e267fb96bf7e2dc4ef5bb2d6ecf5483e9104b5b6
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2022-05-22 16:30:16 +0900

    [김연수]

--- src/main/java/com/bips/reserve/controller/HomeController.java
+++ src/main/java/com/bips/reserve/controller/HomeController.java
@@ -1,10 +1,10 @@
 package com.bips.reserve.controller;
 
 import com.bips.reserve.dto.security.PrincipalDetails;
-        import lombok.extern.slf4j.Slf4j;
-        import org.springframework.security.core.annotation.AuthenticationPrincipal;
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
